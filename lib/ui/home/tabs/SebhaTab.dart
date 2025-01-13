0d368ef319eddbc4c318034f10be08906804d631
Ahme43ban <shaabanahmed531@gmail.com>
2025-01-13 16:26:54 +0200

    +finish SebhaTab.dart + finish logic counter + reformat all code + finish logic rotate

--- lib/ui/home/tabs/SebhaTab.dart
+++ lib/ui/home/tabs/SebhaTab.dart
@@ -1,17 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:islami/style/StringsManger.dart';
 
 import '../../../style/assetsmanger.dart';
+import '../../../style/colorsmanger.dart';
 
-class Sebhatab extends StatelessWidget {
-  const Sebhatab({super.key});
+class Sebhatab extends StatefulWidget {
+  @override
+  State<Sebhatab> createState() => _SebhatabState();
+}
 
+class _SebhatabState extends State<Sebhatab> {
+  int time = 0;
+  int sebhaConter = 0;
+  double rotation = 0.0;
+  //String currentZekr = StringsManger.firstZekr;
+  List<String> currentzekr = [
+    StringsManger.firstZekr,
+    StringsManger.secondtZekr,
+    StringsManger.thirdZekr,
+    StringsManger.FourthZekr
+  ];
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-        decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage(AssetsManger.sebhaback),fit: BoxFit.fitWidth)
-        )
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(AssetsManger.sebhaback), fit: BoxFit.fitWidth)),
+      child: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: Column(
+            children: [
+              Align(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    AssetsManger.logo,
+                    scale: 1.25,
+                  )),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                StringsManger.mainzekr,
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 30,
+                    fontFamily: 'janna',
+                    color: ColorManger.searchText),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Image.asset(
+                AssetsManger.sebhahead,
+              ),
+              GestureDetector(
+                onTap: () {
+                  RotationAngel();
+                  conter(sebhaConter);
+                },
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Transform.rotate(
+                      angle: rotation,
+                      child: Image.asset(
+                        AssetsManger.SebhaBody,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          currentzekr[time],
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 30,
+                              fontFamily: 'janna',
+                              color: ColorManger.searchText),
+                        ),
+                        Text(
+                          "${sebhaConter}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 30,
+                              fontFamily: 'janna',
+                              color: ColorManger.searchText),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  void conter(int num) {
+    setState(() {
+      num++;
+      sebhaConter = num;
+      if (sebhaConter == 31) {
+        time == 3 ? time = 0 : time++;
+        sebhaConter = 0;
+      }
+    });
+  }
+
+  void RotationAngel() {
+    setState(() {
+      rotation == 360 ? rotation = 0 : rotation += 180;
+      print(rotation);
+    });
   }
 }
--- lib/ui/home/widgets/SuraWidget.dart
+++ lib/ui/home/widgets/SuraWidget.dart
@@ -7,53 +7,68 @@ import 'package:islami/ui/sura_details/screen/suradetails_screen.dart';
 class Surawidget extends StatelessWidget {
   SuraModel sura;
   final void Function() addToRecent;
-  Surawidget({required this.sura,required this.addToRecent});
+  Surawidget({required this.sura, required this.addToRecent});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         addToRecent();
-        Navigator.pushNamed(context,SuradetailsScreen.routName,arguments: sura);
+        Navigator.pushNamed(context, SuradetailsScreen.routName,
+            arguments: sura);
       },
       child: Row(
         children: [
-          Stack(alignment: Alignment.center,
+          Stack(
+            alignment: Alignment.center,
             children: [
-              SvgPicture.asset(AssetsManger.suranumber,height: 52,width: 52,),
-              Text(sura.suraNumber.toString(),style: TextStyle(
-                fontFamily: "janna",
-                fontSize: 20,
-                fontWeight: FontWeight.w700,
-                color: Colors.white
-              ),)
-            ],
-          ),
-          SizedBox(width: 24,),
-          Expanded(
-            child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(sura.suraNameEn,style: TextStyle(
+              SvgPicture.asset(
+                AssetsManger.suranumber,
+                height: 52,
+                width: 52,
+              ),
+              Text(
+                sura.suraNumber.toString(),
+                style: TextStyle(
                     fontFamily: "janna",
                     fontSize: 20,
                     fontWeight: FontWeight.w700,
-                    color: Colors.white
-                ),),
-                Text("${sura.versesNumber} verses",style: TextStyle(
-                    fontFamily: "janna",
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white
-                ))
+                    color: Colors.white),
+              )
+            ],
+          ),
+          SizedBox(
+            width: 24,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  sura.suraNameEn,
+                  style: TextStyle(
+                      fontFamily: "janna",
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white),
+                ),
+                Text("${sura.versesNumber} verses",
+                    style: TextStyle(
+                        fontFamily: "janna",
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white))
               ],
             ),
           ),
-          Text(sura.suraNameAr,style: TextStyle(
-              fontFamily: "janna",
-              fontSize: 20,
-              fontWeight: FontWeight.w700,
-              color: Colors.white
-          ),),
+          Text(
+            sura.suraNameAr,
+            style: TextStyle(
+                fontFamily: "janna",
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                color: Colors.white),
+          ),
         ],
       ),
     );
